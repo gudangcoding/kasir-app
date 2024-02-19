e9193916554b738cc4ba0ca2a7cc668391374b7c
gudangcoding <dientrimedia@gmail.com>
2024-02-19 23:30:11 +0700

    komit 1

---
+++ src/components/Hasil.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Col } from 'react-bootstrap'
+
+export default class Hasil extends Component {
+  render() {
+    return (
+        <Col md={2} mt={2}>
+        <h4>Keranjang</h4>
+        <hr/>
+      </Col>
+    )
+  }
+}
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Hasil from "./Hasil";
+import ListCategories from "./ListCategories";
+import Menus from "./Menus";
+
+export { Hasil, ListCategories,Menus };
